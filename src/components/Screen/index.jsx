ffd83ed6f6d10d7fbff648a1fc8aae4e06a30d79
Camilo Nore <68476835+Camilo-Nore@users.noreply.github.com>
2022-01-18 17:15:08 +0000

    Added preview and improving the valid input characters

--- src/components/Screen/index.jsx
+++ src/components/Screen/index.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css';
+
 export default function Screen ({userNumbers, setUserNumbers}){
+
   const handleEqual = () => {
     try{
       setUserNumbers(eval(userNumbers))
@@ -8,13 +10,27 @@ export default function Screen ({userNumbers, setUserNumbers}){
       alert('Check your equation')
     }
   };
+
   const handleChange = (e) =>{
-    setUserNumbers(e.target.value)
+    const equation = e.target.value
+    const regex = /^(\d*[*-/]?\d*){1,}$/
+    regex.test(equation) && setUserNumbers(equation)
   }
+
   const handlePressedKey = (e) =>{
     e.key == 'Enter' && handleEqual()
   }
+
+  const preview = (equation) => {
+    try{
+      return eval(equation)
+    }catch{}
+  }
+
   return(
-    <input type="text" autoFocus className='screen' onChange={handleChange} value={userNumbers || ''} onKeyPress={handlePressedKey}/>
+    <div className='screen'>
+      <input type="text" autoFocus className='screen-input' onChange={handleChange} value={userNumbers || ''} onKeyPress={handlePressedKey}/>
+      <p className='preview-result'>{preview(userNumbers)}</p>
+    </div>
   )
 }
